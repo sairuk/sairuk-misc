a50a36715e7e0f237697adfe4a9f61d3d7c10b06
sairuk <sairuk@ea4e20b0-0ecf-11df-ac28-27e10010dbf6>
2010-06-22 12:43:06 +0000

    Catver support tweaked

--- php/listgen/outputs/catver.php
+++ php/listgen/outputs/catver.php
@@ -4,45 +4,67 @@
 #
 # Create a CatVer file from input. refer http://www.mameworld.info/catver
 
-global $modname, $outfile;
+global $modname,$sessionID,$regionfile,$statusfile,$typefile;
 
 $modname = 'catver';
-$outfile = "$name.ini";
+$regionfile = UPLOADPATH .'/'. $sessionID .'/'. $name."cat_region.ini";
+$statusfile = UPLOADPATH .'/'. $sessionID .'/'. $name."cat_status.ini";
+$typefile = UPLOADPATH .'/'. $sessionID .'/'. $name."cat_type.ini";
 
 function writeout_header()
 {
-	global $xmlhndl;
-	fwrite($xmlhndl , ";;; CatVer (rev. 1) / 11-Sep-05 / $name / http://www.mameau.com/listgen ;;\n\n");
-	fwrite($xmlhndl , "[Category]\n");
+	global $regionfile,$statusfile,$typefile,$regionhndl,$statushndl,$typehndl;
+
+	$regionhndl = @fopen($regionfile,"w");
+	$statushndl = @fopen($statusfile,"w");
+	$typehndl = @fopen($typefile,"w");
+
+
+	fwrite($regionhndl , ";;; CatVer (rev. 1) / 11-Sep-05 / $name / http://www.mameau.com/listgen ;;\n\n");
+	fwrite($regionhndl , "[Category]\n");
+        fwrite($statushndl , ";;; CatVer (rev. 1) / 11-Sep-05 / $name / http://www.mameau.com/listgen ;;\n\n");
+        fwrite($statushndl , "[Category]\n");
+        fwrite($typehndl , ";;; CatVer (rev. 1) / 11-Sep-05 / $name / http://www.mameau.com/listgen ;;\n\n");
+        fwrite($typehndl , "[Category]\n");
 }
 
 function writeout_contents($items) 
 {
 
-	global $xmlhndl;
-	$category = array();
+	global $regionhndl,$statushndl,$typehndl;
+	$catregion = array();
+	$catstatus = array();
+	$cattype = array();
         
         foreach ($items AS $item)
         {
         	include(DOCROOT."/inputs/good-nointro.php");
-			if(!empty($dumpstat)) {	array_push($category, "Status / ".$dumpstat); }
-			if(!empty($langlist)) { array_push($category, "Region / ".$langlist); }			
-			if(!empty($dumptype)) { array_push($category, "Type / ".$dumptype); }
-			if(!empty($dumpfix))  { array_push($category, "Fixed / ".$dumpfix); }
-			if(!empty($translist)) { array_push($category, "Translation / ".$translist); }
+			if(!empty($dumpstat)) {	array_push($catstatus, "Status / ".$dumpstat); }
+			if(!empty($langlist)) { array_push($catregion, "Region / ".$langlist); }			
+			if(!empty($dumptype)) { array_push($cattype, "Type / ".$dumptype); }
+			if(!empty($dumpfix))  { array_push($cattype, "Fixed / ".$dumpfix); }
+			if(!empty($translist)) { array_push($cattype, "Translation / ".$translist); }
 
-			foreach ( $category as $cat ) {
-				fwrite($xmlhndl , $item."=".$cat."\n");
+			foreach ( $catstatus as $cat ) {
+				fwrite($statushndl , $item."=".$cat."\n");
 			}
 
+			foreach ( $catregion as $cat ) {
+                                fwrite($regionhndl , $item."=".$cat."\n");
+                        }
+
+                        foreach ( $cattype as $cat ) {
+                                fwrite($typehndl , $item."=".$cat."\n");
+                        }
         }
-        
 }
 
 function writeout_footer()
 {
-	global $outfile;
-    create_link($outfile);
+	global $regionfile,$statusfile,$typefile;
+	create_link($regionfile);
+	create_link($statusfile);
+	create_link($typefile);
 }
 
 ?>
